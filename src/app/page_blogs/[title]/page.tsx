1c3ffe67dc5e80b9462ac84d2b48bf0a36220ff5
Tomu Maekawa <tomu.m1107@icloud.com>
2025-01-05 23:42:04 +0900

    add dynamicPage

--- src/app/page_blogs/[title]/page.tsx
+++ src/app/page_blogs/[title]/page.tsx
@@ -1,112 +1,24 @@
 import { getBlogData } from "@/lib/microCMS/blogs";
+import type { BlogDataType } from "@/types/microcmsType";
 import React from "react";
+import Blog from "./components/Blog";
+import RightBar from "./components/RightBar";
 
 const page = async ({ params }: { params: Promise<{ title: string }> }) => {
 	const blogs = await getBlogData();
-	// const blogs = [
-	// 	{
-	// 		id: "111",
-	// 		createdAt: "2202020",
-	// 		updatedAt: "951375",
-	// 		publishedAt: "34187634",
-	// 		revisedAt: "3817498",
-	// 		title: "ダミーデータ１",
-	// 		content:
-	// 			"これはダミーです。これはダミーです。これはダミーです。これはダミーです。これはダミーです。これはダミーです。",
-	// 		eyecatch: {
-	// 			url: "/341BA731-0DCD-469D-A8E0-5DD37125D170_1_105_c.jpeg",
-	// 			width: "500",
-	// 			height: "300",
-	// 		},
-	// 		category: {
-	// 			id: "111",
-	// 			createdAt: "111",
-	// 			updatedAt: "111",
-	// 			publishedAt: "111",
-	// 			revisedAt: "111",
-	// 			name: "111",
-	// 		},
-	// 	},
-	// 	{
-	// 		id: "222",
-	// 		createdAt: "2202020",
-	// 		updatedAt: "951375",
-	// 		publishedAt: "34187634",
-	// 		revisedAt: "3817498",
-	// 		title: "ダミーデータ2",
-	// 		content:
-	// 			"これはダミーです。これはダミーです。これはダミーです。これはダミーです。これはダミーです。これはダミーです。",
-	// 		eyecatch: {
-	// 			url: "/341BA731-0DCD-469D-A8E0-5DD37125D170_1_105_c.jpeg",
-	// 			width: "500",
-	// 			height: "300",
-	// 		},
-	// 		category: {
-	// 			id: "222",
-	// 			createdAt: "111",
-	// 			updatedAt: "111",
-	// 			publishedAt: "111",
-	// 			revisedAt: "111",
-	// 			name: "111",
-	// 		},
-	// 	},
-	// 	{
-	// 		id: "333",
-	// 		createdAt: "2202020",
-	// 		updatedAt: "951375",
-	// 		publishedAt: "34187634",
-	// 		revisedAt: "3817498",
-	// 		title: "ダミーデータ3",
-	// 		content:
-	// 			"これはダミーです。これはダミーです。これはダミーです。これはダミーです。これはダミーです。これはダミーです。",
-	// 		eyecatch: {
-	// 			url: "/341BA731-0DCD-469D-A8E0-5DD37125D170_1_105_c.jpeg",
-	// 			width: "500",
-	// 			height: "300",
-	// 		},
-	// 		category: {
-	// 			id: "333",
-	// 			createdAt: "111",
-	// 			updatedAt: "111",
-	// 			publishedAt: "111",
-	// 			revisedAt: "111",
-	// 			name: "111",
-	// 		},
-	// 	},
-	// 	{
-	// 		id: "444",
-	// 		createdAt: "2202020",
-	// 		updatedAt: "951375",
-	// 		publishedAt: "34187634",
-	// 		revisedAt: "3817498",
-	// 		title: "ダミーデータ4",
-	// 		content:
-	// 			"これはダミーです。これはダミーです。これはダミーです。これはダミーです。これはダミーです。これはダミーです。",
-	// 		eyecatch: {
-	// 			url: "/341BA731-0DCD-469D-A8E0-5DD37125D170_1_105_c.jpeg",
-	// 			width: "500",
-	// 			height: "300",
-	// 		},
-	// 		category: {
-	// 			id: "444",
-	// 			createdAt: "111",
-	// 			updatedAt: "111",
-	// 			publishedAt: "111",
-	// 			revisedAt: "111",
-	// 			name: "111",
-	// 		},
-	// 	}
-	// ]
+	const { title } = await params;
+	const decodeTitle = decodeURIComponent(title as string);
 
 	const selectBlog = blogs.find(
-		async (data) => data.title === (await params).title,
-	);
-	console.log("title");
+		(data) => data.title === decodeTitle,
+	) as BlogDataType;
 	return (
-		<div className="mt-32">
-			<h1 className="text-white">{selectBlog?.title}</h1>
-			<h1 className="text-white">{selectBlog?.content}</h1>
-		</div>
+		<main className="pt-16 bg-gradient-to-tr to-base-100 from-green-500">
+			<div className="w-11/12 mx-auto mt-10 flex flex-col md:flex-row justify-between">
+				<Blog blog={selectBlog} />
+				<RightBar />
+			</div>
+		</main>
 	);
 };
 
